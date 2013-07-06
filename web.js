e7cf8c3e0c6601e402d9b58fa6507472a0641104
Premie <jonmahler@gmail.com>
2013-07-06 20:23:37 +0000

    updates web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+var buffer = fs.readFileSync('index.html');
+var string = buffer.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
