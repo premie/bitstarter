f9221deea416cb2d1992f7e4904de0e145029263
premie <jonmahler@gmail.com>
2013-08-01 23:35:11 +0000

    Tried to revert code to pre stripe

--- web.js
+++ web.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var fs = require('fs');
-var stripe = require('stripe');
 
 var buffer = fs.readFileSync('index.html');
 var string = buffer.toString();
@@ -19,23 +18,3 @@ var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Node Server  on port " + port);
 });
-/*
-app.post("/campaigns/1", function(req, res) {
-  stripe.customers.create({
-    card : req.body.stripeToken,
-    email : "...", // customer's email (get it from db or session)
-    plan : "RITEST"
-  }, function (err, customer) {
-    if (err) {
-      var msg = customer.error.message || "unknown";
-      res.send("Error while processing your payment: " + msg);
-    }
-    else {
-      var id = customer.id;
-      console.log('Success! Customer with Stripe ID ' + id + ' just signed up!');
-      // save this customer to your database here!
-      res.send('ok');
-    }
-  });
-});
-*/
